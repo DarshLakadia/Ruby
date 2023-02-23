265e2baee0ed0f75b5e2af76e224d57e34635132
DarshLakadia <darsh.lakadia@bacancy.com>
2023-02-23 11:40:29 +0530

    assignment-2 is added

---
+++ assignment2-5.rb
@@ -0,0 +1,8 @@
+arr = Array(1...100)
+puts arr.include?(100)
+#puts arr
+for i in arr
+	if i % 7 ==0
+		print "#{i} "
+	end
+end
---
+++ assignment2-6.rb
@@ -0,0 +1,23 @@
+puts "6.1"
+
+for i in (1..5).to_a.reverse
+	for j in (1..5) 
+		if (j<=i)
+			print "*"
+		end
+	end
+	puts ""
+end
+
+
+
+puts ""
+puts "6.2"
+for i in 1..5
+	j=1
+	while(j<=i)
+		print j
+		j+=1	
+	end
+	puts ""
+end
